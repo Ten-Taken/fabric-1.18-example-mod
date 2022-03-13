c0a3ad7c3ff084eae655e7bd64b320ec2d04d9bc
tentaken <fleeceofhades@gmail.com>
2022-03-13 15:20:58 -0500

    Add three misc items, Mythril Ingot, Mythril Nugget, Raw Mythril

---
+++ src/main/java/net/tentaken/tutorialmod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.tentaken.tutorialmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import net.tentaken.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final Item MYTHRIL_INGOT = registerItem("mythril_ingot",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item MYTHRIL_NUGGET = registerItem("mythril_nugget",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    public static final Item RAW_MYTHRIL = registerItem("raw_mythril",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registry.ITEM, new Identifier(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for " + TutorialMod.MOD_ID);
+    }
+}
